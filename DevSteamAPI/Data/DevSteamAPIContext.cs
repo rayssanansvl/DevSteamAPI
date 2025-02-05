e8ee3f7a28fbf30bf41c3f0b888f380280241631
Rayssa <rayssa.senaiii@gmail.com>
2025-02-05 16:05:47 -0300

    Adicionando os Controllers Jogos e Categorias

--- DevSteamAPI/Data/DevSteamAPIContext.cs
+++ DevSteamAPI/Data/DevSteamAPIContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using DevSteamAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DevSteamAPI.Data
@@ -9,10 +10,16 @@ namespace DevSteamAPI.Data
         public DevSteamAPIContext(DbContextOptions<DevSteamAPIContext> options) : base(options)
         { }
 
+        //Definir as tabelas do banco de dados
+        public DbSet<Jogo> Jogos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
         //Sobrescrever o metodo OnModelCreating
         protected override void OnModelCreating(ModelBuilder modelbuilder)
         {
             base.OnModelCreating(modelbuilder);
+            modelbuilder.Entity<Jogo>().ToTable("Jogos");
+            modelbuilder.Entity<Categoria>().ToTable("Categorias");
         }
     }
 }
